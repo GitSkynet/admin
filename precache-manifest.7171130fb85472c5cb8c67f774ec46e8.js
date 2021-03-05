05805220995776c7bdab0bf2e2e25dff06350c4c
GitSkynet <carlos.curtido@gmail.com>
2021-03-05 14:14:23 +0100

    Updates

---
+++ precache-manifest.7171130fb85472c5cb8c67f774ec46e8.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9901d78d38e375ca19ebfc3782871028",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "bbcdc8ad8c49dfcdc14c",
+    "url": "/admin/static/css/main.89c7ce01.chunk.css"
+  },
+  {
+    "revision": "abd9594411fc5a038c97",
+    "url": "/admin/static/js/0.da6cb18b.chunk.js"
+  },
+  {
+    "revision": "917e4e14afb320de1f6803eb119e5494",
+    "url": "/admin/static/js/0.da6cb18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d18a3456b3265bda2a",
+    "url": "/admin/static/js/1.cebd1b6d.chunk.js"
+  },
+  {
+    "revision": "6e359ac45458633140d8",
+    "url": "/admin/static/js/10.245767fc.chunk.js"
+  },
+  {
+    "revision": "289a38a5cf315105f127",
+    "url": "/admin/static/js/11.97a68d2a.chunk.js"
+  },
+  {
+    "revision": "784a9c2d23eb57a9cd2a",
+    "url": "/admin/static/js/12.935f3940.chunk.js"
+  },
+  {
+    "revision": "cd94f9518e17755e5563",
+    "url": "/admin/static/js/13.d489f74f.chunk.js"
+  },
+  {
+    "revision": "ff8e440aeb443391804d",
+    "url": "/admin/static/js/14.a538fdf6.chunk.js"
+  },
+  {
+    "revision": "8d56e0fff128aa69e2ef",
+    "url": "/admin/static/js/15.45a3b745.chunk.js"
+  },
+  {
+    "revision": "4447d1fc6819f6f6a71b",
+    "url": "/admin/static/js/16.b7c492b3.chunk.js"
+  },
+  {
+    "revision": "d0cb686fe91f8ed73674",
+    "url": "/admin/static/js/17.21cdefc1.chunk.js"
+  },
+  {
+    "revision": "e87cc90bb011cda79131",
+    "url": "/admin/static/js/4.83c77844.chunk.js"
+  },
+  {
+    "revision": "0fcea004861c56f1cb29effca621e1b1",
+    "url": "/admin/static/js/4.83c77844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981b15731795189c326b",
+    "url": "/admin/static/js/5.3dc705c6.chunk.js"
+  },
+  {
+    "revision": "db67bee1dcd3e011e2b964d80a0a4296",
+    "url": "/admin/static/js/5.3dc705c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89f96969f888cd9e56c",
+    "url": "/admin/static/js/6.cb190245.chunk.js"
+  },
+  {
+    "revision": "6cf54b2be82c59be8b62",
+    "url": "/admin/static/js/7.9768001b.chunk.js"
+  },
+  {
+    "revision": "d01e8be65e28557b4716",
+    "url": "/admin/static/js/8.3c5958d6.chunk.js"
+  },
+  {
+    "revision": "6fc7efbba36e08e93ea7",
+    "url": "/admin/static/js/9.6794dec2.chunk.js"
+  },
+  {
+    "revision": "bbcdc8ad8c49dfcdc14c",
+    "url": "/admin/static/js/main.2a2f3b8b.chunk.js"
+  },
+  {
+    "revision": "39b97aec134784cb544e",
+    "url": "/admin/static/js/runtime-main.7c4d7cd8.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/admin/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/admin/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/admin/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/admin/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "d5f40349fe80fec0be52e90eb66af44a",
+    "url": "/admin/static/media/avatar3.d5f40349.jpg"
+  },
+  {
+    "revision": "ef96b0fd01d526cad7e40ddb5dd2a2c1",
+    "url": "/admin/static/media/bg1.ef96b0fd.jpg"
+  },
+  {
+    "revision": "456b47bce336ad14b0da2b679a1ea9e7",
+    "url": "/admin/static/media/bg2.456b47bc.jpg"
+  },
+  {
+    "revision": "bade0f805119c4e72979c6455fec5e1f",
+    "url": "/admin/static/media/bg3.bade0f80.jpg"
+  },
+  {
+    "revision": "ae3c874d0f8ad41aee2a32dae5fab77b",
+    "url": "/admin/static/media/bg4.ae3c874d.jpg"
+  },
+  {
+    "revision": "d67305229d9cbfe677b1498d5941d5e0",
+    "url": "/admin/static/media/bg5.d6730522.jpg"
+  },
+  {
+    "revision": "e9a0c915319a285fca55ffef3b144456",
+    "url": "/admin/static/media/hero-5.e9a0c915.jpg"
+  },
+  {
+    "revision": "11a8f47e01fe816b8ef48fd9fd3bb810",
+    "url": "/admin/static/media/react.11a8f47e.svg"
+  },
+  {
+    "revision": "5843dac34df329f6ee7a701b6d899cec",
+    "url": "/admin/static/media/stock-1.5843dac3.jpg"
+  },
+  {
+    "revision": "4412962f9ef6dc6c760d2519be24e683",
+    "url": "/admin/static/media/stock-2.4412962f.jpg"
+  },
+  {
+    "revision": "257f5bc02b6104813b1c8cc41a822b38",
+    "url": "/admin/static/media/stock-3.257f5bc0.jpg"
+  }
+]);
